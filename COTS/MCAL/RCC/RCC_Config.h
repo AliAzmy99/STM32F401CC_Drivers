3fb350c65572a52eb895be39463faa1c92d1d867
Ali Azmy <ali.azmy99@eng-st.cu.edu.eg>
2023-07-23 17:27:37 +0300

    Refactorring RCC

--- COTS/MCAL/RCC/RCC_Config.h
+++ COTS/MCAL/RCC/RCC_Config.h
@@ -13,13 +13,26 @@
 
 /*Choose RCC Preferences*/
     /*RCC CLK SRC*/
-#define RCC_CLK_SRC     HSI     /*Options: HSI, HSE, PLL*/
+#define RCC_CLK_SRC     PLL     /*Options: HSI, HSE, PLL*/
 
     /*PLL Configuration*/
-#define PLLM    (u)
-#define PLLN    (u)
-#define PLLP    (u)
-#define PLLQ    (u)
+        /*Choosing PLL Multiplier Parametes*/
+#define PLLM    (2u)        /*Options: 2 --> 63*/
+#define PLLN    (192u)      /*Options: 192 --> 432*/
+#define PLLP    (2u)        /*Options: 2,4,6,8*/
+#define PLLQ    (2u)        /*Options: 2 --> 15*/
+/*ADC_REF_VOLTAGE Reference voltage Options*/
+    /*
+     * ADC_VREF_EXTERNAL		:	External Voltage Reference (reference voltage put through  AREF pin)
+     * ADC_VREF_VCC				:	AVCC with external capacitor at AREF pin
+     * ADC_VREF_INTERNAL1_1V	:	Internal 1.1V Voltage Reference with external capacitor at AREF pin
+     * ADC_VREF_INTERNAL2_56V	:	Internal 2.56V Voltage Reference with external capacitor at AREF pin
+     */
+
+        /*Selecting PLL CLK Src*/
+#define PLL_CLK_SRC     HSI     /*Options: HSI, HSE*/
+    
+
 /*__________________________________________________________________________________________________________________________________________*/
 
 
