782a77f602d5a58986ec3f2d36714f62871e1573
Ali Azmy <ali.azmy99@eng-st.cu.edu.eg>
2023-07-23 17:39:31 +0300

    PLL Config Comment

--- COTS/MCAL/RCC/RCC_Config.h
+++ COTS/MCAL/RCC/RCC_Config.h
@@ -21,13 +21,14 @@
 #define PLLN    (192u)      /*Options: 192 --> 432*/
 #define PLLP    (2u)        /*Options: 2,4,6,8*/
 #define PLLQ    (2u)        /*Options: 2 --> 15*/
-/*ADC_REF_VOLTAGE Reference voltage Options*/
-    /*
-     * ADC_VREF_EXTERNAL		:	External Voltage Reference (reference voltage put through  AREF pin)
-     * ADC_VREF_VCC				:	AVCC with external capacitor at AREF pin
-     * ADC_VREF_INTERNAL1_1V	:	Internal 1.1V Voltage Reference with external capacitor at AREF pin
-     * ADC_VREF_INTERNAL2_56V	:	Internal 2.56V Voltage Reference with external capacitor at AREF pin
-     */
+/*
+ * Calculating PLL Frequencies:
+ * PLL input Freq = Frecuency of PLL input source                   (chosen in RCC_PLL_SRC) 
+ * VCO input freq = PLL input clk freq / PLLM                       (ensure that this freq ranges from 1 to 2 MHz. It is recommended to select a freq of 2 MHz to limit PLL jitter)
+ * VCO output freq = VCO input freq × PLLN                          (ensure that thist freq ranges from 192 to 432 MHz)
+ * PLL general clk output freq = VCO output freq / PLLP             (ensure that this freq does not exceed 84 MHz)
+ * USB,OTG,FS,SDIO,RNG clk output freq = VCO output freq / PLLQ     (ensure that this freq does not exceed 48 MHz)
+ */
 
         /*Selecting PLL CLK Src*/
 #define PLL_CLK_SRC     HSI     /*Options: HSI, HSE*/
