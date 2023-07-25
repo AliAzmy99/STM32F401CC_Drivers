3de4f94b64e0420f1450f428f89c2593812a5b8e
Ali Azmy <ali.azmy99@eng-st.cu.edu.eg>
2023-07-25 13:40:34 +0300

    Adding New Function

--- COTS/MCAL/RCC/RCC_Config.h
+++ COTS/MCAL/RCC/RCC_Config.h
@@ -1,8 +1,6 @@
 /************************************************************************/
 /* SWC  	    : RCC Driver                                            */
 /* Author	    : Ali Azmy                                              */
-/* Version	    : V0.0                                                  */
-/* Date 	    : 21 Jul 2023                                           */
 /* Description  : SWC for Reset and Clock Control                       */
 /************************************************************************/
 
@@ -12,8 +10,8 @@
 
 
 /*Choose RCC Preferences*/
-    /*RCC CLK SRC*/
-#define RCC_CLK_SRC     PLL     /*Options: HSI, HSE, PLL*/
+    /*RCC Initial CLK Src*/
+#define RCC_INIT_CLK_SRC     PLL     /*Options: HSI, HSE, PLL*/
 
     /*PLL Configuration*/
         /*Choosing PLL Multiplier Parametes*/
