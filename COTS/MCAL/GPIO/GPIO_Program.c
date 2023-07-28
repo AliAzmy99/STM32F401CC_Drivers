2a891fb5dc119498909a668948bcd9ce087431c2
Ali Azmy <ali.azmy99@eng-st.cu.edu.eg>
2023-07-28 12:13:10 +0300

    GPIO Template

---
+++ COTS/MCAL/GPIO/GPIO_Program.c
@@ -0,0 +1,21 @@
+/************************************************************************/
+/* SWC  	    : GPIO Driver											*/
+/* Author	    : Ali Azmy												*/
+/* Version	    : V0.1													*/
+/* Description  : SWC for General-Purpose I/Os 							*/
+/************************************************************************/
+
+
+/*Include Needed Library Files*/
+#include "../../LIB/STD_TYPES.h"
+#include "../../LIB/BIT_MATH.h"
+
+/*Include Needed Library Files*/
+#include "GPIO_Interface.h"
+#include "GPIO_Private.h"
+#include "GPIO_Config.h"
+/*__________________________________________________________________________________________________________________________________________*/
+
+
+/*Public Functions Definitions*/
+
