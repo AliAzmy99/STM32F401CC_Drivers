cca2d29a0a4bd1e24f7f179a5041d9b370da94a8
Ali Azmy <ali.azmy99@eng-st.cu.edu.eg>
2023-09-15 15:05:56 +0300

    Main SCH example

---
+++ Main.c
@@ -0,0 +1,34 @@
+#include "COTS/LIB/STD_TYPES.h"
+#include "COTS/LIB/MACROS.h"
+#include "COTS/LIB/BIT_MATH.h"
+
+#include "COTS/MCAL/RCC/RCC_Interface.h"
+#include "COTS/MCAL/GPIO/GPIO_Interface.h"
+#include "COTS/MCAL/STK/STK_Interface.h"
+
+#include "COTS/SRV/SCH/SCH_Interface.h"
+
+int main(void)
+{
+    /*Init System Clock*/
+    RCC_errInitSysClk();
+    
+    /*Enable Clock for GPIOA*/
+    RCC_errEnablePeripheralClk(RCC_PERIPHERAL_GPIOA);
+    
+    /*Configure the Mode of the LEDS Pins as O/P*/
+
+
+    /*Create Tasks*/
+    SCH_errCreateTask(0, 200, APP_LED1,  0);  //200  micro sec
+    SCH_errCreateTask(1, 500, APP_LED2,  1);  //500  micro sec
+    SCH_errCreateTask(2, 100, APP_LED3, 2);  //100   micro sec
+
+    /*Start Scheduling*/
+    SCH_StartOs();
+
+    while(1);
+
+	/*Return*/
+	return 0;
+}
