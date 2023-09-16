add8ab8cb2860884e9e3604160ebbe353dc9a9b2
Ali Azmy <ali.azmy99@eng-st.cu.edu.eg>
2023-09-16 16:03:28 +0300

    adding eff mul and div

--- COTS/LIB/MACROS.h
+++ COTS/LIB/MACROS.h
@@ -31,4 +31,31 @@
 /*__________________________________________________________________________________________________________________________________________*/
 
 
+/*Efficient Multiplication and Division*/
+	/*Multiplication*/
+#define MUL2(Var)			((Var) << 1)
+#define MUL4(Var)			((Var) << 2)
+#define MUL8(Var)			((Var) << 3)
+#define MUL16(Var)			((Var) << 4)
+#define MUL32(Var)			((Var) << 5)
+#define MUL64(Var)			((Var) << 6)
+#define MUL128(Var)			((Var) << 7)
+#define MUL256(Var)			((Var) << 8)
+#define MUL512(Var)			((Var) << 9)
+#define MUL1024(Var)		((Var) << 10)
+
+	/*Division*/
+#define DIV2(Var)			((Var) >> 1)
+#define DIV4(Var)			((Var) >> 2)
+#define DIV8(Var)			((Var) >> 3)
+#define DIV16(Var)			((Var) >> 4)
+#define DIV32(Var)			((Var) >> 5)
+#define DIV64(Var)			((Var) >> 6)
+#define DIV128(Var)			((Var) >> 7)
+#define DIV256(Var)			((Var) >> 8)
+#define DIV512(Var)			((Var) >> 9)
+#define DIV1024(Var)		((Var) >> 10)
+/*__________________________________________________________________________________________________________________________________________*/
+
+
 #endif /*MACROS_H*/
